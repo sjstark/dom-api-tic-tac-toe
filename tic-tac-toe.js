4494770ea745d52a10595c0ba5f69eeb8d8e65a7
Lane Smit <lanesmit88@outlook.com>
2020-09-03 17:49:05 -0500

    checkFull

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
   let squareVals = [
     ["", "", ""],
     ["", "", ""],
-    ["", "", ""]
+    ["", "", ""],
   ];
 
   let board = document.getElementById("tic-tac-toe-board");
@@ -19,17 +19,16 @@ window.addEventListener("DOMContentLoaded", (event) => {
     if (targetelement.innerHTML !== "") {
       return;
     } else {
-
       let divClass = targetelement.getAttribute("class").split(" ");
       let row = Number(divClass[1].slice(-1)) - 1;
       let col = Number(divClass[2].slice(-1)) - 1;
 
       if (currentPlayer === "O") {
-        squareVals[row][col] = 'O';
+        squareVals[row][col] = "O";
         targetelement.innerHTML =
           '<img src="./images/player-o.svg" class="O"/>';
       } else {
-        squareVals[row][col] = 'X';
+        squareVals[row][col] = "X";
         targetelement.innerHTML =
           '<img src="./images/player-x.svg" class="X"/>';
       }
@@ -37,9 +36,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
       currentPlayer = currentPlayer === "X" ? "O" : "X";
     }
 
-    checkRows();
-    checkCols();
-    // checkDiag();
+checkGameState();
+
   });
 
   let checkGameState = function () {
@@ -47,9 +45,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
     checkRows();
     checkCols();
     checkDiag();
+    checkFull();
+    if(!gameStatus) {
+      declareWinner()
+    }
   };
 
-/*
+  /*
   let squareVals = [
        0    1    2
    0 ["X", "O", "X"], //row 0
@@ -60,10 +62,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   function checkRows() {
     for (let i = 0; i < squareVals.length; i++) {
-      let row = squareVals[i]
-      if ((row[0] === row[1]) && (row[0] === row[2]) && (row[0] !== '')) {
-        gameStatus = row[0]
-        console.log(gameStatus)
+      let row = squareVals[i];
+      if (row[0] === row[1] && row[0] === row[2] && row[0] !== "") {
+        gameStatus = row[0];
+        console.log(gameStatus);
         return;
       }
     }
@@ -73,33 +75,46 @@ window.addEventListener("DOMContentLoaded", (event) => {
     for (let i = 0; i < squareVals.length; i++) {
       let col = [];
       for (let j = 0; j < squareVals.length; j++) {
-        col.push(squareVals[j][i])
+        col.push(squareVals[j][i]);
       }
-      if ((col[0] === col[1]) && (col[0] === col[2]) && (col[0] !== '')) {
-        gameStatus = col[0]
-        console.log(gameStatus)
+      if (col[0] === col[1] && col[0] === col[2] && col[0] !== "") {
+        gameStatus = col[0];
+        console.log(gameStatus);
         return;
       }
     }
-
   }
 
+  function checkDiag() {
+    let diag1 = [];
+    let diag2 = [];
+    for (let i = 0; i < squareVals.length; i++) {
+      diag1.push(squareVals[i][i]);
+      diag2.push(squareVals[i][2 - i]);
+    }
+    if (diag1[0] === diag1[1] && diag1[0] === diag1[2] && diag1[0] !== "") {
+      gameStatus = diag1[0];
+      console.log(gameStatus);
+      return;
+    }
+    if (diag2[0] === diag2[1] && diag2[0] === diag2[2] && diag2[0] !== "") {
+      gameStatus = diag2[0];
+      console.log(gameStatus);
+      return;
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  function checkFull() {
+    for (let i = 0; i < squareVals.length; i++) {
+      for (let j = 0; j < squareVals[i].length; j++) {
+        if (squareVals[i][j] === "") {
+          return false;
+        }
+      }
+    }
+    gameStatus = "none";
+    return true;
+  }
 
   // function getBoardState() {
   //   let boardVals = [];
