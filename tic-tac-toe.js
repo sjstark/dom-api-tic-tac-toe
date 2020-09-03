f1fe8cdeba16fea35902ce2f3b9b2bd87d6e9442
Lane Smit <lanesmit88@outlook.com>
2020-09-03 16:12:17 -0500

    require3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,28 +1,52 @@
-window.addEventListener('DOMContentLoaded', event =>{
+window.addEventListener("DOMContentLoaded", (event) => {
+  let currentPlayer = "X";
 
-  let currentPlayer = 'X';
+  let squareVals = ["", "", "", "", "", "", "", "", ""];
 
-  let squareVals = ['', '', '', '', '', '', '', '', '',]
+  let board = document.getElementById("tic-tac-toe-board");
 
-  let board = document.getElementById('tic-tac-toe-board')
-
-  board.addEventListener('click', event => {
-    console.log(event.target)
-    if (event.target.innerHTML !== '') {
-      return
-    } else {
-    if (currentPlayer === 'O') {
-      event.target.innerHTML = '<img src="./images/player-o.svg" />'
-
-    } else {
-      event.target.innerHTML = '<img src="./images/player-x.svg" />'
+  board.addEventListener("click", (event) => {
+    let targetelement = event.target;
+    if (event.target.nodeName === "IMG") {
+      targetelement = event.target.parentNode;
     }
+    if (targetelement.innerHTML !== "") {
+      return;
+    } else {
+      if (currentPlayer === "O") {
+        targetelement.innerHTML = '<img src="./images/player-o.svg" />';
+      } else {
+        targetelement.innerHTML = '<img src="./images/player-x.svg" />';
+      }
 
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    console.log(currentPlayer)
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+  });
+
+
+  let checkGameState = function( ) {
+    let boardState = getBoardState();
+    checkRows(boardState);
+    checkColls(boardState);
+    checkDiag(boardState);
+
   }
-})
+
+  function getBoardState() {
+    let boardVals = [];
+    let squares = document.querySelectorAll('.square');
+    for ( let i = 0; i < squares.length; i++) {
+
+    }
+  }
+  function checkRows() {
+    for ( let i = 1; i <= 3; i ++) {
+      let row = document.querySelectorAll(`.row-${i}`)
+      let firstVal = row[0].innerHTML;
+    }
+  }
+
+});
 
 
 
-})
