227c846afeedecfcd741c8b6fbefed7f80da60a9
Sam Stark <SamStarkScenic@gmail.com>
2020-09-03 17:34:10 -0400

    checking rows

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,39 +14,57 @@ window.addEventListener("DOMContentLoaded", (event) => {
       return;
     } else {
       if (currentPlayer === "O") {
-        targetelement.innerHTML = '<img src="./images/player-o.svg" />';
+        targetelement.innerHTML = '<img src="./images/player-o.svg" class="O"/>';
       } else {
-        targetelement.innerHTML = '<img src="./images/player-x.svg" />';
+        targetelement.innerHTML = '<img src="./images/player-x.svg" class="X"/>';
       }
 
       currentPlayer = currentPlayer === "X" ? "O" : "X";
     }
+
+    checkRows();
   });
 
 
   let checkGameState = function( ) {
-    let boardState = getBoardState();
-    checkRows(boardState);
-    checkColls(boardState);
-    checkDiag(boardState);
+    // let boardState = getBoardState();
+    checkRows();
+    checkColls();
+    checkDiag();
 
   }
 
-  function getBoardState() {
-    let boardVals = [];
-    let squares = document.querySelectorAll('.square');
-    for ( let i = 0; i < squares.length; i++) {
+  // function getBoardState() {
+  //   let boardVals = [];
+  //   let squares = document.querySelectorAll('.square');
+  //   for ( let i = 0; i < squares.length; i++) {
 
-    }
-  }
+  //   }
+  // }
+
   function checkRows() {
     for ( let i = 1; i <= 3; i ++) {
       let row = document.querySelectorAll(`.row-${i}`)
       let firstVal = row[0].innerHTML;
+      let secondVal = row[1].innerHTML;
+      let thirdVal = row[2].innerHTML;
+
+      if ((firstVal === secondVal) &&
+      (firstVal === thirdVal) &&
+      (firstVal !== '')) {
+
+        let imageClass = row[0].lastElementChild.getAttribute('class');
+
+        if (imageClass === 'O') {
+          console.log('O!!!!')
+          return 'O won!'
+        }
+        if (imageClass === 'X') {
+          console.log('X!!!!')
+          return 'X won!'
+        }
+      }
     }
   }
 
 });
-
-
-
